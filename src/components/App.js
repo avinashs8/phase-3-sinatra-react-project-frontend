c7e588587a82b58cc43b87432b7191ea6d16b8dc
avinash singh <avinash8.as@gmail.com>
2023-05-25 21:11:15 -0400

    add routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import '../App.css';
 import NavBar from './NavBar';
 import Home from './Home';
+import PharmacyDetails from './PharmacyDetails';
+import PharmacyList from './PharmacyList';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/pharmacies'>
+          <PharmacyList />
+        </Route>
+        <Route exact path='/pharmacies/:id' component={PharmacyDetails}/>
+      </Switch>
     </div>
   );
 }
